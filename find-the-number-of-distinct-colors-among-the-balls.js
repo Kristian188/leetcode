803c720f974a4b41b047ec6e79aa2ee9db798755
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-02-07 16:50:08 +0200

    find-the-number-of-distinct-colors-among-the-balls

---
+++ find-the-number-of-distinct-colors-among-the-balls.js
@@ -0,0 +1,29 @@
+const limit = 1;
+const queries = [[0,1],[1,4],[1,1],[1,4],[1,1]];
+
+function check(limit, queries) {
+    let uniqueColors = []; 
+    let colorsStored = new Map();
+    let colorCount = new Map();
+
+    for (let i = 0; i < queries.length; i++) {
+        let ball = queries[i][0];
+        let newColor = queries[i][1];
+        if (colorsStored.has(ball)) {
+            let oldColor = colorsStored.get(ball);
+            colorCount.set(oldColor, (colorCount.get(oldColor) || 0) - 1);
+
+            if (colorCount.get(oldColor) === 0) {
+                colorCount.delete(oldColor);
+            }
+        }
+
+        colorsStored.set(ball, newColor);
+        colorCount.set(newColor, (colorCount.get(newColor) || 0) + 1);
+        uniqueColors.push(colorCount.size);
+    }
+
+    return uniqueColors;
+}
+
+console.log(check(limit, queries)); 
