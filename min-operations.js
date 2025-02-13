6300cda7f0eef41df3a48efbbcaf55cb730bdf76
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-02-13 21:16:59 +0200

    add description

--- min-operations.js
+++ min-operations.js
@@ -1,9 +1,15 @@
+//Select the two smallest integers x and y from nums.
+// Remove x and y from nums.
+// Insert (min(x, y) * 2 + max(x, y)) at any position in the array.
+// Note that you can only apply the described operation if nums contains at least two elements.
+// Return the minimum number of operations needed so that all elements of the array are greater than or equal to k.
+
 const nums = [1,1,2,4,9];
 const k = 20;
 
 
 
-var minOperations = function(nums, k) {
+function minOperations(nums, k) {
     
     nums.sort((a, b) => a - b);
     let operations = 0;
