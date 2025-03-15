73f7ecce1584be333d03043ab3cfee3a4ed5aa5e
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-03-15 21:25:10 +0200

    single-element task

---
+++ single-element.js
@@ -0,0 +1,20 @@
+const nums = [1,1,2,2,3];
+
+function singleNumber(nums) {
+    const mapNums = new Map();
+    for(let n of nums) {
+
+        if(!mapNums.has(n)) {
+            mapNums.set(n, 1)
+
+        } else {
+            mapNums.delete(n);
+        }
+    }
+    let result = [...mapNums.keys()][0];
+    return result
+    
+
+}
+
+console.log(singleNumber(nums))
